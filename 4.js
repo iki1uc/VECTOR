906281b5a0730d98fd8bab453ebe2ede3702359b
iki1uc <entertrainer@web.de>
2026-07-23 14:12:57 +0200

    Update 4.js

--- 4.js
+++ 4.js
@@ -3,13 +3,13 @@ function GEO3D_COLOR(f){
     const p = (i)=> d[(f+i)%d.length];
 
     return `
-GEO‑3D COLOR
-X: <span style="color:#f00">${p(1)}</span> <span style="color:#f00">${p(2)}</span>
-Y: <span style="color:#0f0">${p(3)}</span> <span style="color:#0f0">${p(4)}</span>
-Z: <span style="color:#00f">${p(5)}</span> <span style="color:#00f">${p(1)}</span>
-H: <span style="color:#ff0">${p(2)}</span>
-B: <span style="color:#0ff">${p(3)}</span>
-T: <span style="color:#f0f">${p(4)}</span>
-M: <span style="color:#fff">${p(5)}</span>
+GEO‑4D COLOR
+X: <span style="color:#f00">${p(1)}</span>
+Y: <span style="color:#0f0">${p(2)}</span>
+Z: <span style="color:#00f">${p(3)}</span>
+W: <span style="color:#ff0">${p(4)}</span>
+
+4D‑ROTATION:
+${" ".repeat(f%20)}↻
 `;
 }
