c93f1d862efd4f52581bafb4ad7e528b88658145
iki1uc <entertrainer@web.de>
2026-07-23 14:30:00 +0200

    Update VECTOR.render.js

--- VECTOR.render.js
+++ VECTOR.render.js
@@ -5,26 +5,25 @@ window.VECTOR_ASCII = {
         this.frame++;
         const f = this.frame;
 
-        const out = {
-            geo4d: GEO4D(f),
-            geo5d: GEO5D(f),
-            universe: GEO_UNIVERSE(f),
-            gta_chase: GTA6_CHASE(f),
-            gta_story: GTA6_STORY(f),
-            amiga: AMIGA_MEGA(f),
-            sound: SOUND_FFT(f)
-        };
+        const out = `
+${GEO4D(f)}
+
+${GEO5D(f)}
+
+${GEO_UNIVERSE(f)}
+
+${GTA6_CHASE(f)}
+
+${GTA6_STORY(f)}
+
+${AMIGA_MEGA(f)}
+
+${SOUND_FFT(f)}
+`;
 
         const box = document.getElementById("vectorASCII");
         if(box){
-            box.innerHTML =
-                out.geo4d + "\n\n" +
-                out.geo5d + "\n\n" +
-                out.universe + "\n\n" +
-                out.gta_chase + "\n\n" +
-                out.gta_story + "\n\n" +
-                out.amiga + "\n\n" +
-                out.sound;
+            box.innerHTML = out;
         }
     }
 };
