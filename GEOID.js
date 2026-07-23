f8176b5488fb2295d432b79545893341d26e9062
iki1uc <entertrainer@web.de>
2026-07-23 14:40:57 +0200

    Update GEOID.js

--- GEOID.js
+++ GEOID.js
@@ -1,27 +1,15 @@
-function GEO_ID(mode, f){
-    const map = {
-        koop: ID_KOOP,
-        neutral: ID_NEUTRAL,
-        pook: ID_POOK
-    };
-
-    const id = map[mode];
-    const info = id.info();
-
+function GEOID(mode, f){
     const spin = ["●","○","∙","•","◦"];
     const p = spin[f % spin.length];
 
     return `
 GEO‑ID (${mode.toUpperCase()})
-${p}  ${p}  ${p}
+${p} ${p} ${p}
 
-ID‑INFO:
-${info}
-
-DIMENSION‑LINK:
-4D → 5D → ∞D
+QI: ${p}
+IQQ: ${p}${p}
 
 STATE:
-SYNC OK
+ID‑SYNC OK
 `;
 }
