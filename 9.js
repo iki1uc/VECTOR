c01dc4d31751c25e6d2c5962b0d81cc53960306b
iki1uc <entertrainer@web.de>
2026-07-23 14:14:00 +0200

    Update 9.js

--- 9.js
+++ 9.js
@@ -3,8 +3,8 @@ function SOUND_BARS(f){
     const b = (i)=> bars[(f+i)%bars.length];
 
     return `
-SOUND‑BARS
+SOUND‑BARS (BASS BOOST)
 ${b(1)} ${b(3)} ${b(5)} ${b(7)} ${b(2)} ${b(4)} ${b(6)}
-${b(2)} ${b(4)} ${b(6)} ${b(1)} ${b(3)} ${b(5)} ${b(7)}
+${b(7)} ${b(6)} ${b(5)} ${b(4)} ${b(3)} ${b(2)} ${b(1)}
 `;
 }
