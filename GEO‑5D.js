ac0b308dc26b8467a40d8c9b1baca9bfe14ccb1c
iki1uc <entertrainer@web.de>
2026-07-23 14:25:18 +0200

    Create GEO‑5D.js

---
+++ GEO‑5D.js
@@ -0,0 +1,14 @@
+function GEO5D(f){
+    const d = ["●","○","·","∙","•","◦"];
+    const p = (i)=> d[(f+i)%d.length];
+
+    return `
+GEO‑5D
+X: ${p(1)}   Y: ${p(2)}
+Z: ${p(3)}   W: ${p(4)}
+V: ${p(5)}
+
+5D‑SPIN:
+${" ".repeat(f%25)}⟳
+`;
+}
