dff59b74ae561ad839f02904182ff7032f94f333
iki1uc <entertrainer@web.de>
2026-07-23 14:26:37 +0200

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,10 +1,11 @@
-function SOUND_BASS(f){
+function SOUND_FFT(f){
     const bars = ["▁","▂","▃","▄","▅","▆","▇","█"];
     const b = (i)=> bars[(f+i)%bars.length];
 
     return `
-SOUND‑BARS (BASS BOOST)
+SOUND‑FFT‑BASS
 ${b(1)} ${b(3)} ${b(5)} ${b(7)} ${b(2)} ${b(4)} ${b(6)}
 ${b(7)} ${b(6)} ${b(5)} ${b(4)} ${b(3)} ${b(2)} ${b(1)}
+${b(4)} ${b(2)} ${b(6)} ${b(1)} ${b(7)} ${b(3)} ${b(5)}
 `;
 }
