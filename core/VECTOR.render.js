4895310c50ce1c397654c9afca44d8d92aabc714
iki1uc <entertrainer@web.de>
2026-07-29 01:19:30 +0200

    Update VECTOR.render.js

--- core/VECTOR.render.js
+++ core/VECTOR.render.js
@@ -1,5 +1,21 @@
+import { WHIRL_CORE } from "./WHIRL/WHIRL_CORE.js";
+
 class VECTOR_RENDER {
 
+    // mathematischer WHIRL-Renderer
+    compute(v, t){
+        const whirl = WHIRL_CORE(v, t);
+
+        return {
+            x: v.x + whirl.S1,
+            y: v.y + whirl.S2,
+            z: v.z + whirl.S3,
+            seq: whirl.seq,
+            whirl
+        };
+    }
+
+    // UI-Renderer für 27 Felder
     render27(list){
         const out = document.getElementById("vector27");
         out.innerHTML = "";
@@ -10,18 +26,21 @@ class VECTOR_RENDER {
             div.innerText = `${i+1} · ${item}`;
             div.style.top = (i*40)+"px";
             out.appendChild(div);
-export function VECTOR_RENDER(v) {
-    return {
-        x: v.x + v.whirl.S1,
-        y: v.y + v.whirl.S2,
-        z: v.z + v.whirl.S3,
-        seq: v.whirl.seq
-    };
-}
-
         });
+    }
 
+    // UI-Renderer für WHIRL-Sequenzen
+    renderWhirl(v){
+        const out = document.getElementById("whirlSeq");
+        out.innerHTML = "";
 
+        v.seq.forEach((s, i)=>{
+            const div = document.createElement("div");
+            div.className = "seq";
+            div.innerText = `SEQ ${i+1}: ${s}`;
+            div.style.left = (i*40)+"px";
+            out.appendChild(div);
+        });
     }
 }
 
