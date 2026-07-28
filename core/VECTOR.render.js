7a42abd5d76eb4fb70f4109d07a693521cd53022
iki1uc <entertrainer@web.de>
2026-07-29 01:17:43 +0200

    Update VECTOR.render.js

--- core/VECTOR.render.js
+++ core/VECTOR.render.js
@@ -10,7 +10,18 @@ class VECTOR_RENDER {
             div.innerText = `${i+1} · ${item}`;
             div.style.top = (i*40)+"px";
             out.appendChild(div);
+export function VECTOR_RENDER(v) {
+    return {
+        x: v.x + v.whirl.S1,
+        y: v.y + v.whirl.S2,
+        z: v.z + v.whirl.S3,
+        seq: v.whirl.seq
+    };
+}
+
         });
+
+
     }
 }
 
